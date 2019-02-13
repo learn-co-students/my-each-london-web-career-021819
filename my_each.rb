2f796328a711a1020e5ab753f9b7046978c26add
fernando <fernando_lopezc@live.com>
2019-02-13 08:34:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+collection = [1,2,3,4]
+def my_each(collection)
+if block_given?
+  counter = 0
+  while counter<collection.length
+    yield collection[counter]
+    counter+=1
+    end # put argument(s) here
+collection
+else
+  return
 end
+end
+
+my_each(collection) do
+  |element| puts element end
